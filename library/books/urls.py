f92c425dfa0684b628c35bce61e8a440621128d4
aswin.m <aswinmurugesh94@gmail.com>
2013-08-08 01:09:47 +0530

    search books falicitated

--- library/books/urls.py
+++ library/books/urls.py
@@ -4,5 +4,9 @@ from books import views
 urlpatterns = patterns('',
 		url(r'login/welcome/books/?$',views.index),
 		url(r'login/welcome/books/add/?$', views.add),
+		url(r'login/welcome/books/delete/?$', views.searchdelete),
+		url(r'login/welcome/books/search/?$', views.search),
+		url(r'login/welcome/books/delete/display/?$', views.display),
+		url(r'login/welcome/books/search/display/?$', views.display),
 		)
 
--- library/books/views.py
+++ library/books/views.py
@@ -4,6 +4,7 @@ from django.template import Context, RequestContext, loader
 from models import Book
 from forms import BookAddForm
 
+flag = 0
 def index(request):
 	t = loader.get_template('books_index.html')
 	c = RequestContext(request)
@@ -33,4 +34,27 @@ def add(request):
 		form = BookAddForm()
 		context = RequestContext(request)
 		return render_to_response('add_book.html' , {'form': form }, context )
+
+def search(request):
+	t = loader.get_template('search_book.html')
+	c = RequestContext( request )
+	return HttpResponse(t.render(c))
+
+def searchdelete(request):
+	global flag
+	flag = 1
+	t = loader.get_template('search_book.html')
+	c = RequestContext( request )
+	return HttpResponse(t.render(c))
+
+def display(request):
+	search_details = request.POST
+	print search_details
+	result = Book.objects.filter(name = search_details['name'])
+	print len(result)
+	if not result:
+		return HttpResponse("Book Not Found")
+	t = loader.get_template('book_display_delete.html')
+	c = RequestContext(request , { 'j' : flag , 'booklist' : result })
+	return HttpResponse(t.render(c))
 # Create your views here.
